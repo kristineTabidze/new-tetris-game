5db9b8672277f611e132834b926e44f57b0ca502
kristineTabidze <qristinatabidze@gmail.com>
2020-12-13 17:04:01 +0400

    start modifying

---
+++ Tetris/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Tetris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pauseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.referenceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label1.Location = new System.Drawing.Point(321, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.75F);
+            this.label2.Location = new System.Drawing.Point(321, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(484, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pauseToolStripMenuItem,
+            this.restartToolStripMenuItem,
+            this.referenceToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // pauseToolStripMenuItem
+            // 
+            this.pauseToolStripMenuItem.Name = "pauseToolStripMenuItem";
+            this.pauseToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pauseToolStripMenuItem.Text = "Pause";
+            this.pauseToolStripMenuItem.Click += new System.EventHandler(this.OnPauseButtonClick);
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.restartToolStripMenuItem.Text = "Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.OnAgainButtonClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(326, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // referenceToolStripMenuItem
+            // 
+            this.referenceToolStripMenuItem.Name = "referenceToolStripMenuItem";
+            this.referenceToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.referenceToolStripMenuItem.Text = "Reference";
+            this.referenceToolStripMenuItem.Click += new System.EventHandler(this.OnInfoPressed);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 561);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Tetris";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pauseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolStripMenuItem referenceToolStripMenuItem;
+    }
+}
+
---
+++ Tetris/Shape.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class Shape
+    {
+
+     
+
+        public int x;
+        public int y;
+        public int[,] matrix;
+        public int[,] nextMatrix;
+        public int sizeMatrix;
+        public int sizeNextMatrix;
+
+        public int[,] IBlock = new int[4, 4]{
+            {1,1,1,1  },
+            {0,0,0,0  },
+            {0,0,0,0  },
+            {0,0,0,0  },
+        };
+
+        public int[,] SBlock = new int[3, 3]{
+            {0,0,0 },
+            {0,2,2 },
+            {2,2,0 },
+        };
+
+        public int[,] ZBlock = new int[3, 3]{
+            {0,0,0 },
+            {4,4,0 },
+            {0,4,4 },
+        };
+
+        public int[,] TBlock = new int[3, 3]{
+            {0,0,0 },
+            {3,3,3 },
+            {0,3,0 },
+        };
+
+       
+        public int[,] OBlock = new int[2, 2]{
+            {5,5 },
+            {5,5 },
+        };
+
+        public int[,] JBlock = new int[3, 3]{
+            {0,0,0 },
+            {6,0,0 },
+            {6,6,6},
+        };
+
+        public int[,] LBlock = new int[3, 3]{
+            {0,0,0 },
+            {0,0,7 },
+            {7,7,7 },
+        };
+
+
+        public Shape(int _x,int _y)
+        {
+            x = _x;
+            y = _y;
+            matrix = GenerateMatrix();
+            sizeMatrix = (int)Math.Sqrt(matrix.Length);
+            nextMatrix = GenerateMatrix();
+            sizeNextMatrix = (int)Math.Sqrt(nextMatrix.Length);
+        }
+
+        public void ResetShape(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+            matrix = nextMatrix;
+            sizeMatrix = (int)Math.Sqrt(matrix.Length);
+            nextMatrix = GenerateMatrix();
+            sizeNextMatrix = (int)Math.Sqrt(nextMatrix.Length);
+        }
+
+        public int[,] GenerateMatrix()
+        {
+            int[,] _matrix = IBlock;
+            Random r = new Random();
+            switch (r.Next(1, 8))
+            {
+                case 1:
+                    _matrix = IBlock;
+                    break;
+                case 2:
+                    _matrix = SBlock;
+                    break;
+                case 3:
+                    _matrix = TBlock;
+                    break;
+                case 4:
+                    _matrix = ZBlock;
+                    break;
+                case 5:
+                    _matrix = OBlock;
+                    break;
+                case 6:
+                    _matrix = JBlock;
+                    break;
+                case 7:
+                    _matrix = LBlock;
+                    break;
+            }
+            return _matrix;
+        }
+
+        public void RotateShape()
+        {
+            int[,] tempMatrix = new int[sizeMatrix,sizeMatrix];
+            for(int i = 0; i < sizeMatrix; i++)
+            {
+                for (int j = 0; j < sizeMatrix; j++)
+                {
+                    tempMatrix[i, j] = matrix[j, (sizeMatrix - 1) - i];
+                }
+            }
+            matrix = tempMatrix;
+            int offset1 = (8 - (x + sizeMatrix));
+            if (offset1 < 0)
+            {
+                for (int i = 0; i < Math.Abs(offset1); i++)
+                    MoveLeft();
+            }
+            
+            if (x < 0)
+            {
+                for (int i = 0; i < Math.Abs(x)+1; i++)
+                    MoveRight();
+            }
+
+        }
+
+        public void MoveDown()
+        {
+            y++;
+        }
+        public void MoveRight()
+        {
+            x++;
+        }
+        public void MoveLeft()
+        {
+            x--;
+        }
+    }
+}
