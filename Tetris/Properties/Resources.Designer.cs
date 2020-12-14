762f0ef259fdab07ef0bea2844471443e30337fa
kristineTabidze <qristinatabidze@gmail.com>
2020-12-15 02:02:40 +0400

    delete useless coments

--- Tetris/Properties/Resources.Designer.cs
+++ Tetris/Properties/Resources.Designer.cs
@@ -79,15 +79,5 @@ namespace Tetris.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap nicin_deniz_suyunu_icemiyoruz {
-            get {
-                object obj = ResourceManager.GetObject("nicin-deniz-suyunu-icemiyoruz", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
